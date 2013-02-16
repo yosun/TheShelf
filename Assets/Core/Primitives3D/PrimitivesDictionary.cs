c953d02f9f24b6baa2daa1073685c852b379ab83
I. Yosun Chang <yosun@nusoy.com>
2013-02-15 23:17:02 -0800

    1.0.3 added labels and dictation shows teapot if you say Utah Teapot as well

--- Assets/Core/Primitives3D/PrimitivesDictionary.cs
+++ Assets/Core/Primitives3D/PrimitivesDictionary.cs
@@ -7,7 +7,7 @@ public class PrimitivesDictionary : MonoBehaviour {
 		s = s.ToLower();
 		if(s=="cylinder")return "Cylinder";
 		else if(s=="pyramid"||s=="pyramid")return "Pyramid";
-		else if(s=="kapok"||s=="teapot")return "Teapot";
+		else if(s=="kapok"||s=="teapot"||s=="utah teapot"||s=="you tag teapot")return "Teapot";
 		else if(s=="tetrahedron"||s=="tetrahedral"||s=="tetrahedral on")return "Tetrahedron";
 		else if(s=="icosahedron")return "Icosahedron";
 		else if(s=="dodecahedron"||s=="doug decahedron")return "Dodecahedron";
